a00a179d5aa57d6b85371563e6704199b7821bcf
Christopher Stock <email@christopherstock.de>
2018-01-12 14:06:23 +0100

    Setup npm packages and primal js structure and entry point.

---
+++ src/component/Main.tsx
@@ -0,0 +1,28 @@
+
+    import * as React from 'react';
+    import { Image, ScrollView, Text } from 'react-native';
+
+    export class Main extends React.Component
+    {
+        render()
+        {
+            return (
+                <ScrollView>
+                    <Image
+                        source={ { uri: 'https://i.chzbgr.com/full/7345954048/h7E2C65F9/' } }
+                        style={ { width: 320, height:180 } }
+                    />
+                    <Text>
+                        On iOS, a React Native ScrollView uses a native UIScrollView.
+                        On Android, it uses a native ScrollView.
+
+                        On iOS, a React Native Image uses a native UIImageView.
+                        On Android, it uses a native ImageView.
+
+                        React Native wraps the fundamental native components, giving you
+                        the performance of a native app, plus the clean design of React.
+                    </Text>
+                </ScrollView>
+            );
+        }
+    }
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+
+    /*******************************************************************************************************************
+    *   Being invoked on application startup.
+    *
+    *   TODO WEAK image loaders: image-URLs in css and tsx not optimal ..?
+    *   TODO WEAK Params for webpack in package.json in order to build different apps?
+    *   TODO WEAK Add cucumber tests?
+    *******************************************************************************************************************/
+    window.onload = function()
+    {
+
+    };
+
+    /*******************************************************************************************************************
+    *   Being invoked on application destruction.
+    *******************************************************************************************************************/
+    window.onunload = function()
+    {
+    };
